a5afdc2db86db0b887b69ed6832d70d984398a42
Elhakmteto <121831442+Elhakmteto@users.noreply.github.com>
2024-07-10 06:34:21 +0300

    XZero7

---
+++ action.js
@@ -0,0 +1,10 @@
+	function facebook() {
+	window.open("https://www.facebook.com/profile.php?id=61561824932346", '_blank');
+	}
+	
+	function openLink(url) {
+	window.open(url, '_blank');
+	}
+	
+	function openHTMLPage(pageUrl) {
+	window.location.href = pageUrl;}
